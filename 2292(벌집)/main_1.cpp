cd9009f269250599f5be80151662a3c4e74d7be5
dhr777 <dhrtmdgh1000@naver.com>
2020-12-15 22:13:47 +0900

    2292

--- 2292(벌집)/main_1.cpp
+++ 2292(벌집)/main_1.cpp
@@ -1,1 +1,25 @@
 #include<iostream>
+
+int main()
+{
+	using namespace std;
+	int n;
+	cin >> n;
+
+    int count = 2; //i 가 count가 되면 res++
+	int plus = 6; // 가중치가 더해지면 숫자가 6개씩 더 많아짐
+	int res = 1; // i가 1일때 결과는 1이므로 1로 초기화 
+
+	for (int i = 2; i <= n; i++) //i가 1일때 반목문을 필요가 없기때문에 2부터 시작 
+	{
+		if(i==count)
+		{
+			count = count + plus;
+			plus = plus + 6;
+			res++;
+		}
+		
+	}
+	cout << res;
+	return 0;
+}
--- 2292(벌집)/main_2.cpp
+++ 2292(벌집)/main_2.cpp
@@ -1,24 +1,24 @@
 #include<iostream>
 
-int main()
+int ~main()
 {
 	using namespace std;
 	int n;
 	cin >> n;
+    
+    int count = 2; 
+	int plus = 6; 
+	int res = 1; 
 
-    int count = 2; //i 가 count가 되면 res++
-	int plus = 6; // 가중치가 더해지면 숫자가 6개씩 더 많아짐
-	int res = 1; // i가 1일때 결과는 1이므로 1로 초기화 
-
-	for (int i = 2; i <= n; i++) //i가 1일때 반목문을 필요가 없기때문에 2부터 시작 
+	while (true) S
 	{
-		if(i==count)
+		if (n < count)break; //n이 범위에 있으면 끝
+		else //없으면 더해주기
 		{
-			count = count + plus;
-			plus = plus + 6;
+			count = count+plus;
+			plus += 6;
 			res++;
 		}
-		
 	}
 	cout << res;
 	return 0;
