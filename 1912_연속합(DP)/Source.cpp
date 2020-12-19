ea42377254712786da3993402019d23b1f989eb8
dhr777 <dhrtmdgh1000@naver.com>
2020-12-19 19:23:40 +0900

    1912

---
+++ 1912_연속합(DP)/Source.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+/*
+2	1  -4	3	4 - 4	6	5  -5
+2	3  -1	3	7	3	9	14	9  //분할 최대값
+*/
+int main() 
+{
+	using namespace std;
+
+	int n;
+	int arr[100000];
+	int sum[100000];
+	cin >> n;
+	
+	for (int i = 0; i < n; i++)cin >> arr[i]; //입력
+
+	sum[0] = arr[0];
+	int max = sum[0];
+    
+	for (int i = 1; i < n; i++)
+	{
+		sum[i] = arr[i] > arr[i] + sum[i - 1] ? arr[i] : arr[i] + sum[i - 1]; //부분합 점화식
+		max = max > sum[i] ? max : sum[i]; //최대값 찾기
+	}
+
+	cout << max << endl;;
+	
+	
+	return 0;
+}
