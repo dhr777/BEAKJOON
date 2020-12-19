47f8191b48a49877a38d83aa9618dca63910de1a
dhr777 <dhrtmdgh1000@naver.com>
2020-12-19 14:09:52 +0900

    2292

---
+++ 2292_벌집(수학)/main_2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+int ~main()
+{
+	using namespace std;
+	int n;
+	cin >> n;
+    
+    int count = 2; 
+	int plus = 6; 
+	int res = 1; 
+
+	while (true) S
+	{
+		if (n < count)break; //n이 범위에 있으면 끝
+		else //없으면 더해주기
+		{
+			count = count+plus;
+			plus += 6;
+			res++;
+		}
+	}
+	cout << res;
+	return 0;
+}
